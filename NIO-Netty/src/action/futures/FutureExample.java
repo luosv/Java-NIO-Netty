5374cc434792b3d7d9672152c06485a239ffdcc4
Luosw <luo_shaowei@outlook.com>
2016-12-06 17:08:03 +0800

    format again

--- NIO-Netty/src/action/futures/FutureExample.java
+++ NIO-Netty/src/action/futures/FutureExample.java
@@ -13,30 +13,26 @@ public class FutureExample {
 
     public static void main(String[] args) throws Exception {
         ExecutorService executor = Executors.newCachedThreadPool();
-        Runnable task1 = new Runnable() {
-            @Override
-            public void run() {
-                // do something
-                System.out.println("i am task1...");
-            }
+        Runnable task1 = () -> {
+            // do something
+            System.out.println("i am task1...");
         };
-        Callable<Integer> task2 = new Callable<Integer>() {
-            @Override
-            public Integer call() throws Exception {
-                // do something
-                return 100;
-            }
+        Callable<Integer> task2 = () -> {
+            // do something
+            return 100;
         };
         Future<?> f1 = executor.submit(task1);
         Future<Integer> f2 = executor.submit(task2);
         System.out.println("task1 is completed? " + f1.isDone());
         System.out.println("task2 is completed? " + f2.isDone());
         // waiting task1 completed
+        //noinspection LoopStatementThatDoesntLoop
         while (f1.isDone()) {
             System.out.println("task1 completed.");
             break;
         }
         // waiting task2 completed
+        //noinspection LoopStatementThatDoesntLoop
         while (f2.isDone()) {
             System.out.println("task2 completed.");
             break;
